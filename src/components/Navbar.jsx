388804e3b5e6fd070d078b0b619f6ee06c7d0079
aamahi <amahi.wp@gmail.com>
2023-12-10 20:30:56 +0600

    feat: nav ui desing for mobile device

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react'
 import {styles} from "../styles.js";
 import {Link} from "react-router-dom";
-import {logo} from "../assets/index.js";
+import {close, logo, menu} from "../assets/index.js";
 import {navLinks} from "../constants/index.js";
 const Navbar = () => {
   const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(false);
   return (
       <nav className={`${styles.paddingX} w-full flex-items-center py-5 fixed top-0 z-20 bg-primary`}>
         <div className={'w-full flex justify-between items-center max-w-7xl mx-auto'}>
@@ -21,6 +22,25 @@ const Navbar = () => {
                   </li>
               ))}
           </ul>
+          <div className='sm:hidden flex flex-1 justify-end items-center'>
+              <img src={!toggle ? close : menu} alt='menu' className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={()=> setToggle(!toggle)} />
+              <div
+                  className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140] z-10 rounded-xl`}>
+                  <ul className={'list-none flex justify-end items-start flex-col gap-4'}>
+                      {navLinks.map((link) => (
+                          <li key={link.id}
+                              className={`${active === link.title ? "text-white" : "text-gray-400"} font-poppins font-medium text-[16px] cursor-pointer`}
+                              onClick={() => {
+                                  setActive(link.title)
+                                  setToggle(!toggle);
+                              }}
+                          >
+                              <a href={`#${link.id}`}>{link.title}</a>
+                          </li>
+                      ))}
+                  </ul>
+              </div>
+          </div>
         </div>
       </nav>
   )
