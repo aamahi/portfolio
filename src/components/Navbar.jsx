49e8c086687a4b2d5ebb40110b07017b253547b6
aamahi <amahi.wp@gmail.com>
2023-12-13 17:51:18 +0600

    feat: hero section UI design compleate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const   Navbar = () => {
               ))}
           </ul>
           <div className='sm:hidden flex flex-1 justify-end items-center'>
-              <img src={!toggle ? close : menu} alt='menu' className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={()=> setToggle(!toggle)} />
+              <img src={toggle ? close : menu} alt='menu' className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={()=> setToggle(!toggle)} />
               <div
                   className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140] z-10 rounded-xl`}>
                   <ul className={'list-none flex justify-end items-start flex-col gap-4'}>
