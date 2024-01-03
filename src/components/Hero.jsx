1c3af2a2fc52c10d740ac6b30203f78a554b98f2
aamahi <amahi.wp@gmail.com>
2024-01-03 16:22:51 +0600

    fix: home url update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
                         I develop user interfaces, web<br className='sm:block hidden' />
-                        web applications and Mobile Applications.
+                        applications and Mobile Applications.
                     </p>
                 </div>
             </div>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/3D-Portfolio/"
+  base: "/3D-Portfolio/",
+  build: { chunkSizeWarningLimit: 1600}
 })
